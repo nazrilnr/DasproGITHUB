81af51908e0fdd9afc2e55f7db685896e4f50da0
MuhammadNazrilNurRahman <mnazrilnurrahman@gmail.com>
2023-10-04 09:01:38 +0700

    commit pertama saya

---
+++ git.java
@@ -0,0 +1,13 @@
+/**
+ * git
+ */
+public class git {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+
+        int c = a + b;
+        System.out.println("" + c);
+    }
+}
